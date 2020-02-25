c65416955ec6951d05124650233710bc18389c06
Patrick Kosterman <patrick@passkit.com>
2020-02-25 12:01:44 +0000

    Encrypted Link Generator: - Compiled binaries for different OS. - Readme and documentation.

--- README.md
+++ README.md
@@ -1,2 +1,73 @@
 # encrypted-link-generator
-Golang tool to generate encrypted links from a CSV.
+Basic CSV parser that generates encrypted PassKit links from a CSV, that can be distributed to your customers. Takes an input file, output destination, base URL and encryption key.
+
+Generating links is free. A pass record is only created once someone clicks on the link; so this is a very
+economical way to generate all your unique pass URLs, that already contain the encrypted pass data, and distribute them to your customers.
+
+The tool is quite fast - it generated over 600,000 links in less than 7 seconds on a Dual Core MacBook Pro with 16GB RAM.
+
+_Important: please note that the links will only work if you project is set public (fig 1). For private projects you can only create passes via the API or Admin Portal._
+
+##How to use (TDLR):
+* Either compile (`go build src/main.go`) yourself or download any of the pre-compiled executables from the `bin` folder (you may need to `chmod 755` to run it).
+* Get your key for your project from the PassKit portal: https://app.passkit.com. You can find the key in your Project Distribution Settings page (fig 2).
+* Get your landing page url from your Project Distribution Settings page (fig 3).
+* Create your CSV file (you can find a sample in the examples folder).
+* Ensure the CSV file contains the correct [headers](#usable-field-names), otherwise we won't be able to map it to the correct pass field.
+* Run the tool: `/bin/encrypted-link-generator-osx  -inFile examples/example-data.csv -outFile out.csv -baseURL https://pskt.io/c/wrsynr -encryptionKey f33332e108e3e5e040924d7dd7651f6f54b242525bf4e8733ea12ac3538af755` (replace values with your own)
+* Open up out.csv and see the links added against each record.
+* You can now distribute the links to your customers. 
+
+##Images
+Figure 1:
+![Figure 1](https://passkit.com/images/github/passkit-public-setting.png "PassKit Project Settings")
+
+Figure 2:
+![Figure 2](https://passkit.com/images/github/passkit-key.png "PassKit Distribution Settings - Project Key")
+
+Figure 3:
+![Figure 3](https://passkit.com/images/github/passkit-project-url.png "PassKit Distribution Settings - Project URL")
+
+
+## Command Parameters
+`-inFile`: The path to your CSV with data.
+`-outFile`: The filename that the program creates & outputs to (if the file exists it will be overwritten).
+`-baseURL`: Your project URL (can get from the PassKit Portal).
+`-encryptionKey`: Your projects encryption key (can get from the PassKit Portal).
+
+##Usable Field Names
+The following fields are currently supported. These are the field names to use as headers in your CSV.
+
+### Person fields
+*person.surname
+*person.forename
+*person.otherNames
+*person.salutation
+*person.suffix
+*person.displayName
+*person.gender
+*person.dateOfBirth
+*person.emailAddress
+*person.mobileNumber
+
+### Member specific fields
+*members.tier.name
+*members.member.externalId
+*members.member.points
+*members.member.tierPoints
+*members.member.secondaryPoints
+*members.member.groupingIdentifier
+*members.member.profileImage
+
+### Generic fields
+*universal.optInt
+
+### UTM tracking parameters
+*utm_source
+*utm_medium
+*utm_name
+*utm_term
+*utm_content
+
+##Support
+The CSV tool works with all published PassKit protocols: members & coupons (soon to be released). 
---
+++ src/main.go
@@ -0,0 +1,154 @@
+package main
+
+import (
+	"bytes"
+	"crypto/aes"
+	"crypto/cipher"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/csv"
+	"encoding/hex"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	inputFile := flag.String("in", "", "path to input csv file.")
+	outputFile := flag.String("out", "", "path to output csv file (cannot be the same as input).")
+	landingPageURL := flag.String("url", "", "landing page base url.")
+	key := flag.String("key", "", "project encryption key.")
+	flag.Parse()
+
+	if *inputFile == "" || *outputFile == "" || *landingPageURL == "" || *key == "" {
+		fmt.Println("Invalid parameters. Usage:\n  -in [path to input csv]\n  -out [path to output csv]\n  -url [landing page base url]\n  -encryptionKey [project encryption key]")
+		os.Exit(1)
+	}
+
+	if inputFile == outputFile {
+		log.Fatal("Error: input filename must be different to output filename")
+	}
+
+	k, err := hex.DecodeString(*key)
+	if err != nil {
+		log.Fatalf("Error: key [%s] is not a valid hexadecimal string", *key)
+	}
+
+	*landingPageURL = strings.Trim(*landingPageURL, "/")
+
+	// setup reader
+	csvIn, err := os.Open(*inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	r := csv.NewReader(csvIn)
+	defer csvIn.Close()
+
+	// setup writer
+	csvOut, err := os.Create(*outputFile)
+	if err != nil {
+		log.Fatal("Unable to open output")
+	}
+	w := csv.NewWriter(csvOut)
+	defer csvOut.Close()
+
+	var fields []string
+	// handle header
+	rec, err := r.Read()
+	if err != nil {
+		log.Fatal(err)
+	}
+	for i, fieldName := range rec {
+		if i == 0 {
+			// Remove any byte order marker
+			fieldName = string(bytes.Trim([]byte(fieldName), "\xef\xbb\xbf"))
+		}
+		fields = append(fields, fieldName)
+	}
+	rec = append(rec, "smartUrl")
+	if err = w.Write(rec); err != nil {
+		_ = csvOut.Close()
+		_ = os.Remove(*outputFile)
+		log.Fatal(err)
+	}
+
+	records := 0
+	for {
+		rec, err = r.Read()
+		if err != nil {
+			if err == io.EOF {
+				log.Printf("Generation complete: %d records processed and written to %s", records, *outputFile)
+				break
+			}
+			_ = csvOut.Close()
+			_ = os.Remove(*outputFile)
+			log.Fatal(err)
+		}
+		if len(rec) != len(fields) {
+			_ = csvOut.Close()
+			_ = os.Remove(*outputFile)
+			log.Fatalf("Error: all rows must be the same length")
+		}
+		record := map[string]string{}
+		for i, fieldValue := range rec {
+			record[fields[i]] = fieldValue
+		}
+		records++
+		jsonBytes, err := json.Marshal(record)
+		if err != nil {
+			_ = csvOut.Close()
+			_ = os.Remove(*outputFile)
+			log.Fatalf("Error: could not create record JSON: %v", err)
+		}
+
+		var iv [aes.BlockSize]byte
+		_, err = io.ReadFull(rand.Reader, iv[:])
+		if err != nil {
+			_ = csvOut.Close()
+			_ = os.Remove(*outputFile)
+			log.Fatalf("Error: could not generate iv: %v", err)
+		}
+
+		encrypted, err := encrypt(k, iv[:], jsonBytes)
+		if err != nil {
+			log.Fatalf("Error: could not encrypt data: %v", err)
+		}
+		b64 := base64.RawURLEncoding.EncodeToString(encrypted)
+		url := fmt.Sprintf("%s?data=%s&iv=%x", *landingPageURL, b64, iv)
+		rec = append(rec, url)
+		if err = w.Write(rec); err != nil {
+			_ = csvOut.Close()
+			_ = os.Remove(*outputFile)
+			log.Fatal(err)
+		}
+		w.Flush()
+	}
+}
+
+func encrypt(key, iv, data []byte) ([]byte, error) {
+	padded, err := pkcs7Pad(data)
+	if err != nil {
+		return nil, err
+	}
+	c, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+	cbc := cipher.NewCBCEncrypter(c, iv)
+	cbc.CryptBlocks(padded, padded)
+	return padded, nil
+}
+
+// pkcs7Pad appends padding.
+func pkcs7Pad(data []byte) ([]byte, error) {
+	padlen := 1
+	for ((len(data) + padlen) % aes.BlockSize) != 0 {
+		padlen = padlen + 1
+	}
+	pad := bytes.Repeat([]byte{byte(padlen)}, padlen)
+	return append(data, pad...), nil
+}
